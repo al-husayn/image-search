33d75aefcf5855e2da0f25a6e749297618f2a65b
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2023-10-16 16:11:34 +0000

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Search } from "./components";
+
+function App() {
+  return (
+    <div className="flex justify-center align-middle items-center">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
