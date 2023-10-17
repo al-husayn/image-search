9656316df948128d2b2b41ed2b73a73cc9506f6d
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2023-10-17 12:01:00 +0000

    export default components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export {default as Search} from './Search';
+export { default as LoadingMessage } from "./LoadingMessage";
+export { default as ImageList } from "./ImageLists";
+export { default as ErrorMessage } from "./ErrorMessage";
