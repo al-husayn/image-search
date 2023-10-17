4f077d4cb72bf706a6a788dfadd1334067244d48
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2023-10-17 12:00:01 +0000

    loading component added

---
+++ src/components/LoadingMessage.tsx
@@ -0,0 +1,8 @@
+// LoadingMessage.tsx
+import React from "react";
+
+const LoadingMessage: React.FC = () => {
+  return <div className="text-center text-O200">Loading...</div>;
+};
+
+export default LoadingMessage;
