135c824523a6639d550587bfa0a4d92b27cffe36
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2023-10-17 11:54:19 +0000

    add error component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+// ErrorMessage.tsx
+import React from "react";
+
+const ErrorMessage: React.FC = () => {
+  return <div className="text-center text-red-500">Error fetching data</div>;
+};
+
+export default ErrorMessage;
